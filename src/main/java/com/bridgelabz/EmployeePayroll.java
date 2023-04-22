fc791191124f01cae27bf19211eca4af1a4a25cf
naveenkumargorakala <nicenaveengorakala@gmail.com>
2023-04-22 22:23:37 +0530

    Added: Create a table And Retrieve data

--- src/main/java/com/bridgelabz/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/EmployeePayroll.java
@@ -1,43 +1,37 @@
 package com.bridgelabz;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Enumeration;
 
 public class EmployeePayroll {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
 
         String dbUrl = "jdbc:mysql://localhost:3306/payroll_service";
         String user = "root";
         String pass = "825131@Nh";
 
-        Connection conn;
-//        try {
-//            String s = "com.mysql.jdbc.Driver";
-//            Class.forName("com.mysql.jdbc.Driver");
-//            System.out.println("Driver Loaded");
-//        } catch (ClassNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
+//        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbUrl, user, pass);
 
-        listDrivers();
-        try {
-            conn = DriverManager.getConnection(dbUrl, user, pass);
-            System.out.println("connection successfullyy..." + conn);
-        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-            e.printStackTrace();
-        }
-    }
 
-    private static void listDrivers() {
-        Enumeration<Driver> driverList = DriverManager.getDrivers();
-        while (driverList.hasMoreElements()) {
-            Driver driverClass = (Driver) driverList.nextElement();
-            System.out.println("  " + driverClass.getClass().getName());
+        Statement stmt = conn.createStatement();
+        ResultSet resultSet = stmt.executeQuery("select * from employee ");
+
+        System.out.println("Created table Successfully...!");
+
+
+        while (resultSet.next()) {
+            // Retrieve by column name
+            System.out.print("ID: " + resultSet.getInt("emp_id"));
+            System.out.print(", Name: " + resultSet.getString("emp_name"));
+            System.out.print(", Salary: " + resultSet.getDouble("salary"));
+            System.out.print(", Date: " + resultSet.getDate("start_date"));
+            System.out.println(", Email: "+ resultSet.getString("emp_email"));
+            System.out.println();
+
+
         }
+        conn.close();
     }
 }
