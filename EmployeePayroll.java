50eef985d1ff786cec95f4a2343ec3f454bdbef0
naveenkumargorakala <121222968+naveenkumargorakala@users.noreply.github.com>
2023-04-23 08:42:39 +0530

    Add:Update With PreparedStatement

--- EmployeePayroll.java
+++ EmployeePayroll.java
@@ -1,10 +1,10 @@
 package com.bridgelabz;
 
 import java.sql.*;
-
+import java.util.Enumeration;
 
 public class EmployeePayroll {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException,NullPointerException {
 
 
         String dbUrl = "jdbc:mysql://localhost:3306/payroll_service";
@@ -15,22 +15,6 @@ public class EmployeePayroll {
         Connection conn = DriverManager.getConnection(dbUrl, user, pass);
         Statement stmt = conn.createStatement();
 
-//        ResultSet resultSet = stmt.executeQuery("select * from employee ");
-//
-//        System.out.println("Created table Successfully...!");
-
-
-//        while (resultSet.next()) {
-//            // Retrieve by column name
-//            System.out.print("ID: " + resultSet.getInt("emp_id"));
-//            System.out.print(", Name: " + resultSet.getString("emp_name"));
-//            System.out.print(", Salary: " + resultSet.getDouble("salary"));
-//            System.out.print(", Date: " + resultSet.getDate("start_date"));
-//            System.out.println(", Email: "+ resultSet.getString("emp_email"));
-//            System.out.println();
-//
-//
-//        }
 
 //        stmt.executeUpdate("create table employee_payroll( id int, name varchar(20),salary double,Basepay double,TaxablePay double,IncomeTax double, NetPay double,gender varchar(10))");
 //        System.out.println("Employee_payroll is Created");
@@ -39,6 +23,17 @@ public class EmployeePayroll {
 //        stmt.executeUpdate("insert into employee_payroll values(3,'Santhu',65000,400000,40000,80000,280000,'M')");
 //        stmt.executeUpdate("insert into employee_payroll values(4,'Neeta',60000,300000,20000,30000,250000,'F')");
 
+
+        // Update teresa basepay to 300000;
+        PreparedStatement preparedStatement=null;
+        try {
+            conn.prepareStatement("update employee_payroll set Basepay=300000 where name='Teresa'");
+            System.out.println("Update Basepay of Teresa...");
+        }
+        catch(NullPointerException e)
+        {
+            e.fillInStackTrace();
+        }
         ResultSet resultSet1=stmt.executeQuery("select * from employee_payroll");
         while(resultSet1.next()){
             System.out.print("Id: "+resultSet1.getInt("id"));
@@ -47,10 +42,7 @@ public class EmployeePayroll {
             System.out.print(", Basepay: "+resultSet1.getDouble("Basepay"));
             System.out.println();
         }
-        // Update teresa basepay to 300000;
-        stmt.executeUpdate("update employee_payroll set Basepay=300000 where name='Teresa'");
-//        System.out.println("Update Basepay of Teresa...");
-        System.out.println("Record Added...!");
+//        System.out.println("Record Added...!");
         conn.close();
     }
 }
